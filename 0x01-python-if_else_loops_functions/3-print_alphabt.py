ba95c205a7ea74db8bbc553d90ffc4b1e51f9ab9
vivianokose <vivianokose@gmail.com>
2023-08-09 16:02:39 -0700

    Python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end="")
+for ch in range(97, 123):
+    if ch != 101 and ch != 113:
+        print("{:c}".format(ch), end='')
