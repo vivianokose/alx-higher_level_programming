74d9ec658fc2a5874868e0d25f08d30455fbd2a4
vivianokose <vivianokose@gmail.com>
2023-09-07 09:45:11 -0700

    prints and inports functions

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+
 # Assign values to variables a and b
 i = 1
 j = 2
@@ -8,8 +9,5 @@ j = 2
 # Import the add function from add_0.py
 from add_0 import add
 
-# Calculate the result using the imported function
-result = add(i, j)
-
 # Print the result in the specified format
-print("{} + {} = {}".format(i, j, result))
+print("{} + {} = {}".format(i, j, result(i, j))
